2188b142814f0a65e434ab1df0b84828fea7961b
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2022-10-13 22:22:17 +0200

    implemented function to find excel files

--- converter/converter.py
+++ converter/converter.py
@@ -1,9 +1,18 @@
 import argparse
+import glob
+
+def get_file_names(folder_name):
+    return glob.glob(folder_name + "*.xlsx")
 
 if __name__ == '__main__':
+    # set up parser
     parser = argparse.ArgumentParser(description='process excel files')
     parser.add_argument("folder_name", 
                         type=str, help="folder containing excel files")
+    
     args = parser.parse_args()
+    file_names = get_file_names(args.folder_name)
+    
+    print(file_names)
     
     print("done")
